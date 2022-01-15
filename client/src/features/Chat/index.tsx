fb3ead53a13643427e7c48b13012ee89960f7322
Olivier Goulet <olivier_goulet@icloud.com>
2022-01-15 12:14:03 -0500

    Add flex grow to chat pane

--- client/src/features/Chat/index.tsx
+++ client/src/features/Chat/index.tsx
@@ -3,218 +3,223 @@ import { ChatMessage } from '../ChatMessage';
 
 export function Chat() {
     return (
-        <div className='flex-col-reverse overflow-y-scroll h-full space-y-3'>
-            <ChatMessage message={{
-                user: {
-                    email: "stevejobs@icloud.com",
-                    firstName: 'Steve',
-                    lastName: 'Jobs',
-                    avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
-                },
-                content: "Hello",
-                timestamp: new Date('2022-01-10')
-            }} />
+        <div className='h-full flex flex-col'>
+            <div className='grow flex-col-reverse overflow-y-scroll space-y-3'>
+                <ChatMessage message={{
+                    user: {
+                        email: "stevejobs@icloud.com",
+                        firstName: 'Steve',
+                        lastName: 'Jobs',
+                        avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
+                    },
+                    content: "Hello",
+                    timestamp: new Date('2022-01-10')
+                }} />
 
-            <ChatMessage message={{
-                user: {
-                    email: "stevejobs@icloud.com",
-                    firstName: 'Steve',
-                    lastName: 'Jobs',
-                    avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
-                },
-                content: "Hello",
-                timestamp: new Date()
-            }} />
-            <ChatMessage message={{
-                user: {
-                    email: "stevejobs@icloud.com",
-                    firstName: 'Steve',
-                    lastName: 'Jobs',
-                    avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
-                },
-                content: "Hello",
-                timestamp: new Date()
-            }} />
-            <ChatMessage message={{
-                user: {
-                    email: "stevejobs@icloud.com",
-                    firstName: 'Steve',
-                    lastName: 'Jobs',
-                    avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
-                },
-                content: "Hello",
-                timestamp: new Date()
-            }} />
-            <ChatMessage message={{
-                user: {
-                    email: "stevejobs@icloud.com",
-                    firstName: 'Steve',
-                    lastName: 'Jobs',
-                    avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
-                },
-                content: "Hello",
-                timestamp: new Date()
-            }} />
-            <ChatMessage message={{
-                user: {
-                    email: "stevejobs@icloud.com",
-                    firstName: 'Steve',
-                    lastName: 'Jobs',
-                    avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
-                },
-                content: "Hello",
-                timestamp: new Date()
-            }} />
-            <ChatMessage message={{
-                user: {
-                    email: "stevejobs@icloud.com",
-                    firstName: 'Steve',
-                    lastName: 'Jobs',
-                    avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
-                },
-                content: "Hello",
-                timestamp: new Date()
-            }} />
-            <ChatMessage message={{
-                user: {
-                    email: "stevejobs@icloud.com",
-                    firstName: 'Steve',
-                    lastName: 'Jobs',
-                    avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
-                },
-                content: "Hello",
-                timestamp: new Date()
-            }} />
-            <ChatMessage message={{
-                user: {
-                    email: "stevejobs@icloud.com",
-                    firstName: 'Steve',
-                    lastName: 'Jobs',
-                    avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
-                },
-                content: "Hello",
-                timestamp: new Date()
-            }} />
-            <ChatMessage message={{
-                user: {
-                    email: "stevejobs@icloud.com",
-                    firstName: 'Steve',
-                    lastName: 'Jobs',
-                    avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
-                },
-                content: "Hello",
-                timestamp: new Date()
-            }} />
-            <ChatMessage message={{
-                user: {
-                    email: "stevejobs@icloud.com",
-                    firstName: 'Steve',
-                    lastName: 'Jobs',
-                    avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
-                },
-                content: "Hello",
-                timestamp: new Date()
-            }} />
-            <ChatMessage message={{
-                user: {
-                    email: "stevejobs@icloud.com",
-                    firstName: 'Steve',
-                    lastName: 'Jobs',
-                    avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
-                },
-                content: "Hello",
-                timestamp: new Date()
-            }} />
-            <ChatMessage message={{
-                user: {
-                    email: "stevejobs@icloud.com",
-                    firstName: 'Steve',
-                    lastName: 'Jobs',
-                    avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
-                },
-                content: "Hello",
-                timestamp: new Date()
-            }} />
-            <ChatMessage message={{
-                user: {
-                    email: "stevejobs@icloud.com",
-                    firstName: 'Steve',
-                    lastName: 'Jobs',
-                    avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
-                },
-                content: "Hello",
-                timestamp: new Date()
-            }} />
-            <ChatMessage message={{
-                user: {
-                    email: "stevejobs@icloud.com",
-                    firstName: 'Steve',
-                    lastName: 'Jobs',
-                    avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
-                },
-                content: "Hello",
-                timestamp: new Date()
-            }} />
-            <ChatMessage message={{
-                user: {
-                    email: "stevejobs@icloud.com",
-                    firstName: 'Steve',
-                    lastName: 'Jobs',
-                    avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
-                },
-                content: "Hello",
-                timestamp: new Date()
-            }} />
-            <ChatMessage message={{
-                user: {
-                    email: "stevejobs@icloud.com",
-                    firstName: 'Steve',
-                    lastName: 'Jobs',
-                    avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
-                },
-                content: "Hello",
-                timestamp: new Date()
-            }} />
-            <ChatMessage message={{
-                user: {
-                    email: "stevejobs@icloud.com",
-                    firstName: 'Steve',
-                    lastName: 'Jobs',
-                    avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
-                },
-                content: "Hello",
-                timestamp: new Date()
-            }} />
-            <ChatMessage message={{
-                user: {
-                    email: "stevejobs@icloud.com",
-                    firstName: 'Steve',
-                    lastName: 'Jobs',
-                    avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
-                },
-                content: "Hello",
-                timestamp: new Date()
-            }} />
-            <ChatMessage message={{
-                user: {
-                    email: "stevejobs@icloud.com",
-                    firstName: 'Steve',
-                    lastName: 'Jobs',
-                    avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
-                },
-                content: "Hello",
-                timestamp: new Date()
-            }} />
-            <ChatMessage message={{
-                user: {
-                    email: "stevejobs@icloud.com",
-                    firstName: 'Steve',
-                    lastName: 'Jobs',
-                    avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
-                },
-                content: "Hello",
-                timestamp: new Date()
-            }} />
+                <ChatMessage message={{
+                    user: {
+                        email: "stevejobs@icloud.com",
+                        firstName: 'Steve',
+                        lastName: 'Jobs',
+                        avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
+                    },
+                    content: "Hello",
+                    timestamp: new Date()
+                }} />
+                <ChatMessage message={{
+                    user: {
+                        email: "stevejobs@icloud.com",
+                        firstName: 'Steve',
+                        lastName: 'Jobs',
+                        avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
+                    },
+                    content: "Hello",
+                    timestamp: new Date()
+                }} />
+                <ChatMessage message={{
+                    user: {
+                        email: "stevejobs@icloud.com",
+                        firstName: 'Steve',
+                        lastName: 'Jobs',
+                        avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
+                    },
+                    content: "Hello",
+                    timestamp: new Date()
+                }} />
+                <ChatMessage message={{
+                    user: {
+                        email: "stevejobs@icloud.com",
+                        firstName: 'Steve',
+                        lastName: 'Jobs',
+                        avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
+                    },
+                    content: "Hello",
+                    timestamp: new Date()
+                }} />
+                <ChatMessage message={{
+                    user: {
+                        email: "stevejobs@icloud.com",
+                        firstName: 'Steve',
+                        lastName: 'Jobs',
+                        avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
+                    },
+                    content: "Hello",
+                    timestamp: new Date()
+                }} />
+                <ChatMessage message={{
+                    user: {
+                        email: "stevejobs@icloud.com",
+                        firstName: 'Steve',
+                        lastName: 'Jobs',
+                        avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
+                    },
+                    content: "Hello",
+                    timestamp: new Date()
+                }} />
+                <ChatMessage message={{
+                    user: {
+                        email: "stevejobs@icloud.com",
+                        firstName: 'Steve',
+                        lastName: 'Jobs',
+                        avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
+                    },
+                    content: "Hello",
+                    timestamp: new Date()
+                }} />
+                <ChatMessage message={{
+                    user: {
+                        email: "stevejobs@icloud.com",
+                        firstName: 'Steve',
+                        lastName: 'Jobs',
+                        avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
+                    },
+                    content: "Hello",
+                    timestamp: new Date()
+                }} />
+                <ChatMessage message={{
+                    user: {
+                        email: "stevejobs@icloud.com",
+                        firstName: 'Steve',
+                        lastName: 'Jobs',
+                        avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
+                    },
+                    content: "Hello",
+                    timestamp: new Date()
+                }} />
+                <ChatMessage message={{
+                    user: {
+                        email: "stevejobs@icloud.com",
+                        firstName: 'Steve',
+                        lastName: 'Jobs',
+                        avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
+                    },
+                    content: "Hello",
+                    timestamp: new Date()
+                }} />
+                <ChatMessage message={{
+                    user: {
+                        email: "stevejobs@icloud.com",
+                        firstName: 'Steve',
+                        lastName: 'Jobs',
+                        avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
+                    },
+                    content: "Hello",
+                    timestamp: new Date()
+                }} />
+                <ChatMessage message={{
+                    user: {
+                        email: "stevejobs@icloud.com",
+                        firstName: 'Steve',
+                        lastName: 'Jobs',
+                        avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
+                    },
+                    content: "Hello",
+                    timestamp: new Date()
+                }} />
+                <ChatMessage message={{
+                    user: {
+                        email: "stevejobs@icloud.com",
+                        firstName: 'Steve',
+                        lastName: 'Jobs',
+                        avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
+                    },
+                    content: "Hello",
+                    timestamp: new Date()
+                }} />
+                <ChatMessage message={{
+                    user: {
+                        email: "stevejobs@icloud.com",
+                        firstName: 'Steve',
+                        lastName: 'Jobs',
+                        avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
+                    },
+                    content: "Hello",
+                    timestamp: new Date()
+                }} />
+                <ChatMessage message={{
+                    user: {
+                        email: "stevejobs@icloud.com",
+                        firstName: 'Steve',
+                        lastName: 'Jobs',
+                        avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
+                    },
+                    content: "Hello",
+                    timestamp: new Date()
+                }} />
+                <ChatMessage message={{
+                    user: {
+                        email: "stevejobs@icloud.com",
+                        firstName: 'Steve',
+                        lastName: 'Jobs',
+                        avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
+                    },
+                    content: "Hello",
+                    timestamp: new Date()
+                }} />
+                <ChatMessage message={{
+                    user: {
+                        email: "stevejobs@icloud.com",
+                        firstName: 'Steve',
+                        lastName: 'Jobs',
+                        avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
+                    },
+                    content: "Hello",
+                    timestamp: new Date()
+                }} />
+                <ChatMessage message={{
+                    user: {
+                        email: "stevejobs@icloud.com",
+                        firstName: 'Steve',
+                        lastName: 'Jobs',
+                        avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
+                    },
+                    content: "Hello",
+                    timestamp: new Date()
+                }} />
+                <ChatMessage message={{
+                    user: {
+                        email: "stevejobs@icloud.com",
+                        firstName: 'Steve',
+                        lastName: 'Jobs',
+                        avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
+                    },
+                    content: "Hello",
+                    timestamp: new Date()
+                }} />
+                <ChatMessage message={{
+                    user: {
+                        email: "stevejobs@icloud.com",
+                        firstName: 'Steve',
+                        lastName: 'Jobs',
+                        avatarLocation: 'https://i1.sndcdn.com/artworks-000354302124-wfd9ky-t500x500.jpg'
+                    },
+                    content: "Hello",
+                    timestamp: new Date()
+                }} />
+            </div>
+            <div className='flex bg-gray-700 rounded-md p-3 text-sm m-4'>
+                <input className='text-gray-400 bg-transparent outline-none' placeholder='Send a message'/>
+            </div>
         </div>
     );
 }
--- client/src/layouts/ChatLayout.tsx
+++ client/src/layouts/ChatLayout.tsx
@@ -10,7 +10,7 @@ interface Props {
 export function ChatLayout({ children }: PropsWithChildren<Props>) {
     return (
         <div className='flex h-full'>
-            <div className='flex-grow dark:bg-gray-900'>
+            <div className='flex-grow dark:bg-gray-900 h-full'>
                 { children }
             </div>
             <div className='dark:bg-gray-800 w-96 flex-none rounded-l-xl'>
@@ -19,7 +19,7 @@ export function ChatLayout({ children }: PropsWithChildren<Props>) {
                     <div className='flex space-x-2'>
                         <TabGroup>
                             <Tab text="Messages"/>
-                            <Tab text="Participants" />
+                            <Tab text="Participants"/>
                         </TabGroup>
                     </div>
                 </div>
