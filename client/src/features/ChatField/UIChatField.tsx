958d1c1ad922ff52fca36ce9c55a96b37fc128cf
Olivier Goulet <olivier_goulet@icloud.com>
2022-02-26 16:01:56 -0500

    Auto grow and shrink

--- client/src/features/ChatField/UIChatField.tsx
+++ client/src/features/ChatField/UIChatField.tsx
@@ -12,15 +12,22 @@ export function UIChatField({ onSendClick, onSendKeyPress, text, setText }: Prop
 
     const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
         const textArea = event.currentTarget;
+        textArea.style.height = "auto"
         textArea.style.height = `${textArea.scrollHeight}px`
 
-        setText(event.target.value);    
+        setText(event.target.value); 
     }
 
     return (
         <div className='mx-4 mb-4'>
-            <div className='flex w-full p-1.5 bg-gray-700 rounded-lg text-gray-400 text-md justify-between'>
-                <textarea value={text} onChange={onChange} onKeyPress={onSendKeyPress} placeholder="Send a message" className='outline-none bg-transparent flex-1 resize-none h-6 my-auto max-h-64 overflow-y-auto' />
+            <div className='flex w-full p-1.5 bg-gray-700 rounded-lg text-gray-400 justify-between'>
+                <textarea 
+                    value={text} 
+                    onChange={onChange} 
+                    onKeyPress={onSendKeyPress} 
+                    placeholder="Send a message" 
+                    className='outline-none bg-transparent flex-1 resize-none my-auto max-h-64 overflow-y-auto' 
+                />
                 <button onClick={onSendClick} className='rounded-lg w-8 h-8 bg-red-500 ml-1'>
                     <PaperPlaneIcon className='text-white h-5 m-auto pr-0.5' />
                 </button>
